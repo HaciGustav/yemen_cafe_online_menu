8f862b1d2a8f1df9bfe6da03ecbeb9e534177b75
MehmetFK <mehmetfkuruldak@gmail.com>
2023-02-26 21:40:10 +0100

    delete auth domain

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -14,7 +14,7 @@ import { toastFailNotify, toastSuccessNotify } from './toastNotify';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
